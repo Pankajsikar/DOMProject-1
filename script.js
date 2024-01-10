598ca6888db9914328cf01853ce656001657aa8b
Pankajsikar <pankajrulyana0003@gmail.com>
2024-01-10 20:26:49 +0530

    my first dom project with geekster

---
+++ script.js
@@ -0,0 +1,10 @@
+let div = document.getElementById("myDiv");
+
+
+div.style.backgroundColor="yellow";
+div.style.margin="20px";
+div.style.padding="10px";
+div.style.fontSize="18px";
+div.style.fontweight="bold";
+div.style.height="200px";
+div.style.width="300px";
